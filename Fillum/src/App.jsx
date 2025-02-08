a04b689e22b6d39f8dc4d4126723e60c88f9dc62
SRB <souhardaroybarman05@gmail.com>
2025-02-08 15:28:52 +0530

    Initial Commit

---
+++ Fillum/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import React from "react";
+import './App.css'
+import Search from "./components/search.jsx";
+
+const App = () => {
+    const [SearchTerm, setSearchTerm] = useState('');
+    return (
+        <main>
+            <div className="pattern"/>
+            <div className="wrapper">
+                <header>
+                    <img src="./hero.png" alt="Hero Banner"/>
+                    <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without a hassle</h1>
+                </header>
+                <Search searchTerm={SearchTerm} setSearchTerm={setSearchTerm}/>
+            </div>
+        </main>
+    )
+}
+export default App
