12a853e9b6f87e918771ffeb4977a289e02ae651
SRB <souhardaroybarman05@gmail.com>
2025-02-08 16:12:33 +0530

    Implement movie fetching functionality with loading and error handling; add Spinner component

--- Fillum/src/App.jsx
+++ Fillum/src/App.jsx
@@ -2,12 +2,60 @@ import { useState , useEffect} from 'react'
 import React from "react";
 import './App.css'
 import Search from "./components/search.jsx";
+import Spinner from './components/Spinner.jsx';
+
+
+const API_BASE_URL ='https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS={
+    method:'GET',
+    headers:{
+        accept: 'application/json',
+        Authorization: `Bearer ${API_KEY}`
+    }
+}
+
+
+
+
 
 const App = () => {
     const [SearchTerm, setSearchTerm] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [movieList, setMovieList] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const fetchMovies = async () => {
+        setIsLoading(true);
+        setErrorMessage('');
+
+        try {
+            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`; 
+
+            const response = await fetch(endpoint,API_OPTIONS);
+
+            if(!response.ok){
+                throw new Error('Failed to fetch movies');
+            }
+
+            const data= await response.json();
+            if(data.response == 'False'){
+                setErrorMessage(data.Error || 'Failed to fetch movies');
+                setMovieList([]);
+                return;
+            }
+            setMovieList(data.results || []);
+        }catch(error){
+            console.error(`Error fetching movies ${error}`);
+            setErrorMessage('Error fetching movies. Please try again later. ');
+        }finally{
+            setIsLoading(false);
+        }
+    }
 
    useEffect(() => {
-
+    fetchMovies();
    }, []);
     
     return (
@@ -17,8 +65,16 @@ const App = () => {
                 <header>
                     <img src="./hero.png" alt="Hero Banner"/>
                     <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without a hassle</h1>
-                </header>
                 <Search searchTerm={SearchTerm} setSearchTerm={setSearchTerm}/>
+                </header>
+                <section className="all-movies">
+                    <h2 className="mt-[40px]">All Movies</h2>
+                    {isLoading ? (<Spinner/>):errorMessage?(<p className='text-red-500'>{errorMessage}</p>):<ul>
+                        {movieList.map((movie) => (
+                            <p key={movie.id} className='text-white'>{movie.title}</p>
+                        ))}
+                        </ul>}
+                </section>
             </div>
         </main>
     )
